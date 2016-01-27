2425998ef6676333758c602dc38a78fd62104ed9
Kkmonster <kkmonster@hotmail.com>
2016-01-28 04:12:50 +0700

    ESP stable version 2.0.0

--- Esp_Levitator.ino
+++ Esp_Levitator.ino
@@ -13,9 +13,9 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
-#define key 0x01
+#define key 0x09
 
-#define buffer_size 20
+#define buffer_size 512
 
 #define LED        16
 #define hall        A0
@@ -53,18 +53,16 @@ float state = 0;
 float state_f = 0;
 float prev_state = 0;
 float prev_state_f = 0;
-float ref = 7.2;
+float ref = 7.0;
 float error = 0;
 float prev_error = 0;
 float error_dot = 0;
 float error_sum = 0;
 
-float kp = 400;
-float ki = 30;
-float kd = 6.53;
+float kp = 500;
+float ki = 20;
+float kd = 5;
 float output  = 0;
-
-uint32_t time_now, time_prev_pid, time_prev_print;
 
 Ticker ticker_PID;
 Ticker ticker_DEGUG;
@@ -120,17 +118,16 @@ void PRE_EEPROM_write_Gain()
   if (EEPROM.read(0) != key)
   {
     EEPROM.write(0, key);
-    EEPROM.commit();
 
     int buffer[6] = {0};
 
-    int16_t g_buffer = 500 * 1.0f;
+    int16_t g_buffer = 500 ;
     buffer[0] =  (int8_t)(g_buffer >> 8);
     buffer[1] =  (int8_t)g_buffer;
-    g_buffer = 20 * 10.0f;
+    g_buffer = 200;
     buffer[2] =  (int8_t)(g_buffer >> 8);
     buffer[3] =  (int8_t)g_buffer;
-    g_buffer = 5.0f * 100.0f;
+    g_buffer = 500 ;
     buffer[4] =  (int8_t)(g_buffer >> 8);
     buffer[5] =  (int8_t)g_buffer;
 
@@ -152,21 +149,33 @@ IPAddress subnet(255, 255, 255, 255);
 
 void setup()
 {
-  Serial.begin(115200);
   pinMode(hall, INPUT);
   pinMode(output, OUTPUT);
   pinMode(LED, OUTPUT);
 
+  delay(1000);
+  Serial.begin(115200);
+
+  char accessPointName[16] = {'\0'};
+  sprintf(accessPointName, "Levitator-%lu", ESP.getChipId());
+  Serial.print("Access Point: ");
+  Serial.println(accessPointName);
+
+  WiFi.mode(WIFI_STA);
   WiFi.disconnect();
+  delay(100);
 
-  WiFi.softAP(ssid, password);
+  WiFi.softAP(accessPointName, "");
+  WiFi.mode(WIFI_AP_STA);
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.softAPIP());
   delay(500);
-  WiFi.softAPConfig(local_ip, gateway, subnet);
-  delay(500);
+  // WiFi.softAPConfig(local_ip, gateway, subnet);
+  // delay(500);
 
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP: ");
-  Serial.println(myIP);
+  // IPAddress myIP = WiFi.softAPIP();
+  // Serial.print("AP IP: ");
+  // Serial.println(myIP);
 
   if ( Udp.begin(localPort) > 0) {
     Serial.print("UDP star port :");
@@ -176,13 +185,12 @@ void setup()
     Serial.println("Error");
 
   }
-  delay(3000);
+  delay(1000);
+
+  EEPROM.begin(10);
 
   PRE_EEPROM_write_Gain();
-
-  EEPROM.begin(512);
   EEPROM_read_Gain();
-
 
   ticker_PID.attach_ms(10, PID);
   // ticker_DEGUG.attach_ms(1000, Debug);
@@ -216,25 +224,24 @@ void Read_UDP()
       {
         switch (tuningData.yawPitchRoll)
         {
-          case 0X01: // yaw
-            // kp = 50.0f;
-            // ki = 0.0f;
-            // kd = 0.0f;
-            break;
-
           case 0X02: // pitch
             kp = (float)tuningData.kp / 1.0f;
             ki = (float)tuningData.ki / 10.0f;
             kd = (float)tuningData.kd / 100.0f;
             EEPROM_write_Gain();
             break;
-
-          case 0X03: // roll
-            // kp = 0.0f;
-            // ki = 0.0f;
-            // kd = 50.0f;
-            break;
         }
+
+
+          Serial.print("Tuning Data ");
+          Serial.print(tuningData.yawPitchRoll == 0X01 ? "(Yaw):" :
+                       tuningData.yawPitchRoll == 0X02 ? "(Pitch):" : "(Roll):");
+          Serial.print(" KP=");
+          Serial.print(kp);
+          Serial.print(" KI=");
+          Serial.print(ki);
+          Serial.print(" KD=");
+          Serial.println(kd);
       }
     }
     else if (data[0] == 0XFC && data[0] == 0XFC) // get tuning data
@@ -249,35 +256,36 @@ void Read_UDP()
         for (tuningData.yawPitchRoll = 0X01; tuningData.yawPitchRoll <= 0X03; tuningData.yawPitchRoll++)
         {
           // sample
-          float kp = 0.0f;
-          float ki = 0.0f;
-          float kd = 0.0f;
+          float _kp = 0.0f;
+          float _ki = 0.0f;
+          float _kd = 0.0f;
 
           switch (tuningData.yawPitchRoll)
           {
             case 0X01: // yaw
-              kp = 50.0f;
-              ki = 0.0f;
-              kd = 0.0f;
-              break;
+             _kp = 0;
+             _ki = 0;
+             _kd = 0;
+                break;
 
             case 0X02: // pitch
-              kp = 0.0f;
-              ki = 50.0f;
-              kd = 0.0f;
-              break;
+             _kp = kp;
+             _ki = ki;
+             _kd = kd;
+                break;
 
             case 0X03: // roll
-              kp = 0.0f;
-              ki = 0.0f;
-              kd = 50.0f;
-              break;
+             _kp = 0;
+             _ki = 0;
+             _kd = 0;
+                break;
+
           }
           // ************
 
-          tuningData.kp = kp * 10.0f;
-          tuningData.ki = ki * 10.0f;
-          tuningData.kd = kd * 10.0f;
+          tuningData.kp = _kp * 1.0f;
+          tuningData.ki = _ki * 10.0f;
+          tuningData.kd = _kd * 100.0f;
           tuningData.checksum = tuningData.yawPitchRoll + tuningData.kp + tuningData.ki + tuningData.kd;
           memcpy(&data, &tuningData, sizeof(TuningData));
 
@@ -300,45 +308,16 @@ void Read_UDP()
           Serial.print(" checksum ");
           Serial.println(tuningData.checksum);
 
-
           Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
           Udp.write(data, sizeof(TuningData));
           Udp.endPacket();
-          delay(400); // don't forget to delay
+          delay(200); // don't forget to delay
         }
 
         Serial.println();
       }
     }
-    else if (data[0] == 0XFE) // trim and control
-    {
-      ControlData controlData = {0};
-      memcpy(&controlData, data, sizeof(ControlData));
-      int16_t checksum = controlData.roll + controlData.pitch + controlData.throttle + controlData.yaw;
-
-      if (controlData.checksum == checksum)
-      {
-        int8_t trimFlag = 0XFF;
-
-        if (controlData.roll == trimFlag && controlData.pitch == trimFlag && controlData.throttle == trimFlag && controlData.yaw == trimFlag)
-        {
-          Serial.println("Trim");
-        }
-        else
-        {
-          Serial.print("Roll=");
-          Serial.print(controlData.roll);
-          Serial.print(" Pitch=");
-          Serial.print(controlData.pitch);
-          Serial.print(" Throttle=");
-          Serial.print(controlData.throttle);
-          Serial.print(" Yaw=");
-          Serial.println(controlData.yaw);
-        }
-      }
-    }
   }
-
 }
 
 void Debug(void)
